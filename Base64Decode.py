6860fbe35e06935917f2faf382c48c0c4efde978
Budtit <z734110414z@163.com>
2019-11-07 22:50:15 +0800

    Base64Encode

--- Base64Decode.py
+++ Base64Decode.py
@@ -6,25 +6,18 @@ base64_str = build_base64_str()
 def base64_decode(string):
     ordstr = ""
     newstr = []
-    destr = ""
+    de_str = ""
     # 去掉等号，转换为二进制，六个一组
     for i in string.replace('=',''):
         ordstr += '{:06}'.format(int(str(bin(base64_str.index(i)).replace('0b', ''))))
     for i in range(0, len(ordstr), 8):
         newstr.append(ordstr[i:i+8])
     for i in range(0,len(newstr)):
-        destr +=  chr(int(newstr[i], 2))
-    return destr
+        de_str +=  chr(int(newstr[i], 2))
+    return de_str
 
 
 
 
 
 
-
-
-
-
-
-if __name__ == '__main__':
-    base64_decode("YWJjZA=")
--- Base64Encode.py
+++ Base64Encode.py
@@ -17,18 +17,18 @@ base64_str = build_base64_str()
 def baseEncode(string):
     ordstr = ''
     newstr = []
-    enstr = ''
+    en_str = ''
     # 转换为二进制去掉b八位一组，不足的前面补齐0
     for i in string:
         ordstr += '{:08}'.format(int(str(bin(ord(i))).replace('0b', '')))
     for i in range(0, len(ordstr),6):
         newstr.append('{:<06}'.format(ordstr[i:i+6]))
     for i in range(0, len(newstr)):
-        enstr += base64_str[int(newstr[i], 2)]
+        en_str += base64_str[int(newstr[i], 2)]
     if(len(ordstr) % 3 == 1):
-        enstr += '=='
+        en_str += '=='
     elif(len(ordstr) % 3 == 2):
-        enstr += '='
-    return enstr
+        en_str += '='
+    return en_str
 
 
